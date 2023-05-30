d2791c74a7be8d3d599c0fd7a2dd6c5b5c5389c5
raquelmms <raqueldemedeirosmartins@gmail.com>
2023-05-30 12:40:57 -0300

    refactor: Add rebase

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,6 @@
 module.exports = {
-    arrowParens: 'avoid',
-    bracketSameLine: true,
-    bracketSpacing: false,
-    singleQuote: true,
-    trailingComma: 'all',
-  };
+  singleQuote: true,
+  trailingComma: 'all',
+  jsxSingleQuote: true,
+  printWidth: 120,
+};
--- ProjectOnboardFrontend/.prettierrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  singleQuote: true,
-  trailingComma: 'all',
-  jsxSingleQuote: true,
-  printWidth: 120,
-};
